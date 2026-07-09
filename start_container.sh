a177406b7cfe21db3d00b2c9c8d779219bfcf5d1
Dinesh8523 <DKollimarla@sskatt.com>
2026-07-09 15:42:37 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-docker pull dineshkolli2002/lion-flask-application:tagname
-docker run -d -p 5000 : 5000 dineshkolli2002/lion-flask-application:tagname
+docker pull dineshkolli2002/lion-flask-application:latest
+docker run -d -p 5000:5000 dineshkolli2002/lion-flask-application:latest
